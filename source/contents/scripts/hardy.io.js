523e1a8c5eb3ce514e7629e3c32e3b88888b849c
Simon Madine <simon@angryrobotzombie.com>
2013-08-14 22:08:15 +0200

    analytics

--- source/contents/scripts/hardy.io.js
+++ source/contents/scripts/hardy.io.js
@@ -7,3 +7,9 @@ function toggleNav(e) {
 }
 document.querySelector('.sub').addEventListener('click', toggleNav, false);
 document.querySelector('.sub').addEventListener('touchstart', toggleNav, false);
+
+var _gaq=[['_setAccount','UA-20281865-6'],['_trackPageview']];
+(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+g.src='//www.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g,s);
+}(document,'script'));
